bfef967349b58ec4825cf6a97e816ea98fe3936e
Erik <erikguimaraesdasilva@gmail.com>
2015-04-01 11:47:53 -0300

    Indent

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function DecodeController($scope) {
 
 	$scope.showtooltip = false;
-	$scope.dataHex = '3074257B80625F0000000002';
+	$scope.dataHex = '350002800004B362DFDC1C35';
 
 	$scope.dataBin 		= Hex2Bin($scope.dataHex);
 	$scope.dataPattern	= GetPattern($scope.dataBin);
+	$scope.dataDecoded	= DecodeData($scope.dataHex);
 
 	$scope.hideTooltip = function () {
 		$scope.showtooltip = false;
@@ -17,124 +18,126 @@ function DecodeController($scope) {
 }
 
 /////////////////// DECODE
+	function DecodeData(Hex){
+		var Bin 	= Hex2Bin(Hex);
+		var Pattern = GetPattern(Bin);
+		var decoded = null;
 
-function DecodeData(Hex){
-	var Bin 	= Hex2Bin(Hex);
-	var Pattern = GetPattern(Bin);
+		switch(Pattern){
+			case "DOD-64":
+			 	decoded = DOD64Decode(Bin);
+			 	break;
+			case "DOD-96":
+			 	decoded = DOD96Decode(Bin);
+			 	break;
+			case "SGTIN-96":
+			 	decoded = SGTIN96Decode(Bin);
+			 	break;
+			case "SSCC-96":
+			 	decoded = SSCC96Decode(Bin);
+			 	break;
+			case "GLN-96":
+			 	decoded = GLN96Decode(Bin);
+			 	break;
+			case "GRAI-96":
+			 	decoded = GRAI96Decode(Bin);
+			 	break;
+			case "GIAI-96":
+			 	decoded = GIAI96Decode(Bin);
+			 	break;
+			case "GID-96":
+			 	decoded = GID96Decode(Bin);
+			 	break;
+		 	default:
+		 		decoded = null;
+		}
 
-	switch(Pattern){
-		case "DOD-64":
-		 	decoded = DOD64Decode(Bin);
-		 	break;
-		case "DOD-96":
-		 	decoded = DOD96Decode(Bin);
-		 	break;
-		case "SGTIN-96":
-		 	decoded = SGTIN96Decode(Bin);
-		 	break;
-		case "SSCC-96":
-		 	decoded = SSCC96Decode(Bin);
-		 	break;
-		case "GLN-96":
-		 	decoded = GLN96Decode(Bin);
-		 	break;
-		case "GRAI-96":
-		 	decoded = GRAI96Decode(Bin);
-		 	break;
-		case "GIAI-96":
-		 	decoded = GIAI96Decode(Bin);
-		 	break;
-		case "GID-96":
-		 	decoded = GID96Decode(Bin);
-		 	break;
-	 	default:
-	 		pattern = null;
-	}
-}
-
-function GetPattern(binaryFull){
-	var header 	= binaryFull.substring(0, 8);
-	var pattern = null;
-
-	switch(header){
-		case "11001110":
-		 	pattern = "DOD-64";
-		 	break;
-		case "11001111":
-		 	pattern = "DOD-96";
-		 	break;
-		case "00110000":
-		 	pattern = "SGTIN-96";
-		 	break;
-		case "00110001":
-		 	pattern = "SSCC-96";
-		 	break;
-		case "00110010":
-		 	pattern = "GLN-96";
-		 	break;
-		case "00110011":
-		 	pattern = "GRAI-96";
-		 	break;
-		case "00110100":
-		 	pattern = "GIAI-96";
-		 	break;
-		case "00110101":
-		 	pattern = "GID-96";
-		 	break;
-	 	default:
-	 		pattern = null;
+		return decoded;
 	}
 
-	return pattern;
-}
+	function GetPattern(binaryFull){
+		var header 	= binaryFull.substring(0, 8);
+		var pattern = null;
+
+		switch(header){
+			case "11001110":
+			 	pattern = "DOD-64";
+			 	break;
+			case "11001111":
+			 	pattern = "DOD-96";
+			 	break;
+			case "00110000":
+			 	pattern = "SGTIN-96";
+			 	break;
+			case "00110001":
+			 	pattern = "SSCC-96";
+			 	break;
+			case "00110010":
+			 	pattern = "GLN-96";
+			 	break;
+			case "00110011":
+			 	pattern = "GRAI-96";
+			 	break;
+			case "00110100":
+			 	pattern = "GIAI-96";
+			 	break;
+			case "00110101":
+			 	pattern = "GID-96";
+			 	break;
+		 	default:
+		 		pattern = null;
+		}
+
+		return pattern;
+	}
 
 /////////////////// CONVERT
-function Bin2Hex(s) {
-    var i, k, part, accum, ret = '';
-    for (i = s.length-1; i >= 3; i -= 4) {
-        part = s.substr(i+1-4, 4);
-        accum = 0;
-        for (k = 0; k < 4; k += 1) {
-            if (part[k] !== '0' && part[k] !== '1') {
-                return { valid: false };
-            }
-            accum = accum * 2 + parseInt(part[k], 10);
-        }
-        if (accum >= 10) {
-            ret = String.fromCharCode(accum - 10 + 'A'.charCodeAt(0)) + ret;
-        } else {
-            ret = String(accum) + ret;
-        }
-    }
-    if (i >= 0) {
-        accum = 0;
-        for (k = 0; k <= i; k += 1) {
-            if (s[k] !== '0' && s[k] !== '1') {
-                return { valid: false };
-            }
-            accum = accum * 2 + parseInt(s[k], 10);
-        }
-        ret = String(accum) + ret;
-    }
-    return ret;
-}
+	function Bin2Hex(s) {
+	    var i, k, part, accum, ret = '';
+	    for (i = s.length-1; i >= 3; i -= 4) {
+	        part = s.substr(i+1-4, 4);
+	        accum = 0;
+	        for (k = 0; k < 4; k += 1) {
+	            if (part[k] !== '0' && part[k] !== '1') {
+	                return { valid: false };
+	            }
+	            accum = accum * 2 + parseInt(part[k], 10);
+	        }
+	        if (accum >= 10) {
+	            ret = String.fromCharCode(accum - 10 + 'A'.charCodeAt(0)) + ret;
+	        } else {
+	            ret = String(accum) + ret;
+	        }
+	    }
+	    if (i >= 0) {
+	        accum = 0;
+	        for (k = 0; k <= i; k += 1) {
+	            if (s[k] !== '0' && s[k] !== '1') {
+	                return { valid: false };
+	            }
+	            accum = accum * 2 + parseInt(s[k], 10);
+	        }
+	        ret = String(accum) + ret;
+	    }
+	    return ret;
+	}
 
-function Hex2Bin(s) {
-    var i, k, part, ret = '';
-    var lookupTable = {
-        '0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100',
-        '5': '0101', '6': '0110', '7': '0111', '8': '1000', '9': '1001',
-        'a': '1010', 'b': '1011', 'c': '1100', 'd': '1101',
-        'e': '1110', 'f': '1111',
-        'A': '1010', 'B': '1011', 'C': '1100', 'D': '1101',
-        'E': '1110', 'F': '1111'
-    };
-    for (i = 0; i < s.length; i += 1) {
-        if (lookupTable.hasOwnProperty(s[i])) {
-            ret += lookupTable[s[i]];
-        } else {
-            return { valid: false };
-        }
-    }
-    return ret;
-}
+	function Hex2Bin(s) {
+	    var i, k, part, ret = '';
+	    var lookupTable = {
+	        '0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100',
+	        '5': '0101', '6': '0110', '7': '0111', '8': '1000', '9': '1001',
+	        'a': '1010', 'b': '1011', 'c': '1100', 'd': '1101',
+	        'e': '1110', 'f': '1111',
+	        'A': '1010', 'B': '1011', 'C': '1100', 'D': '1101',
+	        'E': '1110', 'F': '1111'
+	    };
+	    for (i = 0; i < s.length; i += 1) {
+	        if (lookupTable.hasOwnProperty(s[i])) {
+	            ret += lookupTable[s[i]];
+	        } else {
+	            return { valid: false };
+	        }
+	    }
+	    return ret;
+	}
