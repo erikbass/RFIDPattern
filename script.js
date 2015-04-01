44cffc6b08ed129430e792d95139306c6c2f8e0c
Erik <erikguimaraesdasilva@gmail.com>
2015-04-01 09:49:30 -0300

    Useful Functions to convert

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function DecodeController ($scope) {
+function DecodeController($scope) {
 
 	$scope.showtooltip = false;
-	$scope.value = '3074257B80625F0000000002';
+	$scope.dataHex = '3074257B80625F0000000002';
 
+	$scope.dataBin = Hex2Bin($scope.dataHex);
 
 	$scope.hideTooltip = function () {
 		$scope.showtooltip = false;
@@ -13,3 +14,25 @@ function DecodeController ($scope) {
 		$scope.showtooltip = !$scope.showtooltip;
 	}
 }
+
+
+
+
+//Useful Functions
+function checkBin(n){return/^[01]{1,64}$/.test(n)}
+function checkDec(n){return/^[0-9]{1,64}$/.test(n)}
+function checkHex(n){return/^[0-9A-Fa-f]{1,64}$/.test(n)}
+function pad(s,z){s=""+s;return s.length<z?pad("0"+s,z):s}
+function unpad(s){s=""+s;return s.replace(/^0+/,'')}
+
+//Decimal operations
+function Dec2Bin(n){if(!checkDec(n)||n<0)return 0;return n.toString(2)}
+function Dec2Hex(n){if(!checkDec(n)||n<0)return 0;return n.toString(16)}
+
+//Binary Operations
+function Bin2Dec(n){if(!checkBin(n))return 0;return parseInt(n,2).toString(10)}
+function Bin2Hex(n){if(!checkBin(n))return 0;return parseInt(n,2).toString(16)}
+
+//Hexadecimal Operations
+function Hex2Bin(n){if(!checkHex(n))return 0;return parseInt(n,16).toString(2)}
+function Hex2Dec(n){if(!checkHex(n))return 0;return parseInt(n,16).toString(10)}
